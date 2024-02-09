40850a97fe71424fef70bbe33b5e74b31ae43f6a
psguazz <p.sguazz@gmail.com>
2024-02-09 23:04:02 +0100

    Initial commit

---
+++ lib/graphql_to_json.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "graphql_to_json/version"
+
+module GraphqlToJson
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/graphql_to_json/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GraphqlToJson
+  VERSION = "0.1.0"
+end
