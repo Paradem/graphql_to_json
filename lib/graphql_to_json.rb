e5b9a1246a2b134294b0b44047096492a16d4ed6
psguazz <p.sguazz@gmail.com>
2024-02-09 23:26:40 +0100

    Code!

--- lib/graphql_to_json.rb
+++ lib/graphql_to_json.rb
@@ -4,5 +4,27 @@ require_relative "graphql_to_json/version"
 
 module GraphqlToJson
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.convert(graphql)
+    File.write("#{__dir__}/input.graphql", parse_many(graphql))
+    exec "node #{__dir__}/index.js"
+    File.delete("#{__dir__}/input.graphql")
+
+    File.read("#{__dir__}/output.json")
+  end
+
+  def self.parse_many(gqls)
+    [gqls]
+      .flatten
+      .map { |gql| parse_one(gql) }
+      .join("\n")
+  end
+
+  def self.parse_one(gql)
+    if File.exist?(gql)
+      File.read(gql)
+    else
+      gql.to_s
+    end
+  end
 end
